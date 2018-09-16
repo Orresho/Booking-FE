c0959121ee8b2e7f05a16f07a105a4e5b01ddbef
Orhan Hajderovic <orhanhajder@gmail.com>
2018-09-16 21:12:16 +0200

    move config and update gitignore

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -1,22 +1,5 @@
 import firebase from 'firebase/app'
-
-const devConfig = {
-  apiKey: "AIzaSyCkFl-aonGKLF9mSMfNE8jKRDTs37cf_Bg",
-  authDomain: "orhanblogdev.firebaseapp.com",
-  databaseURL: "https://orhanblogdev.firebaseio.com",
-  projectId: "orhanblogdev",
-  storageBucket: "orhanblogdev.appspot.com",
-  messagingSenderId: "87313294646"
-};
-
-const prodConfig = {
-  apiKey: "AIzaSyBwK0wH9eWoHX5c9zlT1C6_g4-X8OKs8nA",
-  authDomain: "orhanblog-6cff4.firebaseapp.com",
-  databaseURL: "https://orhanblog-6cff4.firebaseio.com",
-  projectId: "orhanblog-6cff4",
-  storageBucket: "orhanblog-6cff4.appspot.com",
-  messagingSenderId: "879095221876"
-};
+import { devConfig, prodConfig } from "../config/firebase";
 
 // Fallback on development config if we don't have a production environment
 const config = process.env.NODE_ENV === "production" 
